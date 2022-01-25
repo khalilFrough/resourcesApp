6e046dd714b8122b3de4ea80d1390edc57c98def
khalil frough <khalil_frough@yahoo.com>
2022-01-25 13:48:18 +1100

    edited the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/contacts'
+  #get 'pages/index'
+  get "contacts", to: 'pages#contacts'
+  root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
