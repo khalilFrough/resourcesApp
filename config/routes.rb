a07779551caa486b1cdc50667387857add3f86cd
khalil frough <khalil_frough@yahoo.com>
2022-07-12 14:30:43 +1000

    created basic login and log out for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   #get 'pages/index'
-  get "contacts", to: 'pages#contacts'
+  get "/contacts", to: 'pages#contacts'
   root to: "pages#index"
   
   resources :users
-  get 'signup', to: 'users#new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
