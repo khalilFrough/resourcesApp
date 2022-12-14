d9cefa6ef0f7761317cc10314a01f20fc96edc6a
khalil frough <khalil_frough@yahoo.com>
2022-12-14 12:09:50 +1100

    Finish user edit, update, index and destry actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:index, :edit, :update]
     before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: :destroy
 
     def index
         @users = User.paginate(page: params[:page])
@@ -40,6 +41,18 @@ class UsersController < ApplicationController
     end
 
     end 
+
+    # destrying a user 
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted successfuly!"
+        redirect_to users_url
+    end
+
+    # Confirms an admin user.
+    def admin_user
+        redirect_to(root_url) unless current_user.admin?
+    end
 
     private 
     def user_params
